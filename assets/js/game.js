77325cb6ca1745edc9b15baa120261cec290f30d
Sihayah <sihaya_a@hotmail.com>
2021-10-18 01:56:07 -0400

    looped fight

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,14 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-if (playerHealth > 0) {
-    console.log("Your player is still alive!");
-  }
 var playerAttack = 10;
+var playerMoney= 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+var fight = function(enemyName) {
     // Alert player that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     // Ask player with they wish to fight or
@@ -58,13 +54,21 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
     // if no (false), ask question again by running fight() again
     else {
-      fight();
+      fight(enemyRobot);
     }
   }
   
   
 };
+// initiate fight
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
+}
+  // fight function statements
 
-fight();
 
-// 
+//Game states
+//  "WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robot
+//      * Defeat each enemy-robot
+//  "LOSE" - Player robot's health is zero or less
